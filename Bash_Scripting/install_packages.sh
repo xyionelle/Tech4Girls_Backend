fb7210348256a8f4eebe93fa057e09e8042f2f57
root <root@Xyion.>
2024-10-15 18:19:21 +0000

    Added install_packages.sh to install fortune, cowsay, and cmatrix

--- Bash_Scripting/install_packages.sh
+++ Bash_Scripting/install_packages.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+# Update the package list
+sudo apt update
+
+#Upgrade the package list
+sudo apt upgrade
+
+# Install the packages fortune, cowsay, and cmatrix
+sudo apt install -y fortune cowsay cmatrix
+
+echo "Installation of fortune, cowsay, and cmatrix is complete."
 
