3292f4469868f816f2e9ef802093a6964afd26f1
xyionelle <47189633+xyionelle@users.noreply.github.com>
2024-10-27 16:13:19 -0600

    Create string_indexing.py

---
+++ Python_Basics/string_indexing.py
@@ -0,0 +1,6 @@
+sentence = "Python is powerful and easy to learn."
+
+print("The length of the sentence is", len(sentence))
+print("The first character of the sentence is", sentence[0])
+print("The last character of the sentence is", sentence[-1])
+print("The word powerful can be printed using slicing", sentence[10:18])
