c7163f0dc63dbbe2afb372e666e68bc27cc082fa
xyionelle <47189633+xyionelle@users.noreply.github.com>
2024-10-27 16:09:03 -0600

    Create string_methods.py

---
+++ Python_Basics/string_methods.py
@@ -0,0 +1,7 @@
+text1 = "First text"
+text2 = "Second text"
+
+print("upper() example", text1.upper())
+print("lower() example", text2.lower())
+print("replace() example", text1.replace("First", "New"))
+print("join() example", " and ".join([text1, text2]))
