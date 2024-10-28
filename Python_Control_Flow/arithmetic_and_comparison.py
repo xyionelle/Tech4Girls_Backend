99359c8b20b8a78453e433e02866719c5a5eba54
xyionelle <47189633+xyionelle@users.noreply.github.com>
2024-10-27 22:35:38 -0600

    Create arithmetic_and_comparison.py

---
+++ Python_Control_Flow/arithmetic_and_comparison.py
@@ -0,0 +1,17 @@
+x = 7
+y = 25
+
+print(f"x + y = {x + y}")
+print(f"x - y = {x - y}")
+print(f"x * y = {x * y}")
+print(f"x / y = {x / y}")
+print(f"x // y = {x // y}")
+print(f"x % y = {x % y}")
+print(f"x ** y = {x ** y}")
+
+print(f"x > y = {x > y}")
+print(f"x < y = {x < y}")
+print(f"x >= y = {x >= y}")
+print(f"x <= y = {x <= y}")
+print(f"x == y = {x == y}")
+print(f"x != y = {x != y}")
