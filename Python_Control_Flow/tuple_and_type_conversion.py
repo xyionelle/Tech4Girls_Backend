9f2e0b63a548a92d58756dcf94787c7fd0a91c89
xyionelle <47189633+xyionelle@users.noreply.github.com>
2024-10-27 22:27:21 -0600

    Create tuple_and_type_conversion.py

---
+++ Python_Control_Flow/tuple_and_type_conversion.py
@@ -0,0 +1,12 @@
+tup_1 = (1, 2, 5, 3, 4, 5)
+
+print("First element of the tuple is", tup_1[0])
+print("Last element of the tuple is", tup_1[-1])
+print("The number of times 5 appears in the tuple is", tup_1.count(5))
+print("The index of 3 in the tuple is", tup_1.index(3))
+
+print("Converting 5 to a float", float(5))
+print("Converting 7.5 to an integer", int(7.5))
+print("Converting '6' to an integer", int('6'))
+
+print("join() example", '-'.join(['a', 'b', 'c']))
